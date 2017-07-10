c8a4c0a4fa358f79ec03cfab3fa2f6c166dd9d1f
Siddharth Shorya <shoryasiddharth90@gmail.com>
2017-07-10 21:50:49 +0530

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -9,28 +9,29 @@ dangal = media.Movie('Dangal',
 
 spiderman_homecoming = media.Movie('Spiderman homecoming',
                                    'SuperHero',
-                                   'http://images.complex.com/complex/images/c_fill,g_center,w_1200/fl_lossy,pg_1,q_auto/uc7ae2rek8ne8ov0ue8e/spider-man-homecoming-poster',
+                                   'http://t0.gstatic.com/images?q=tbn:ANd9GcT8W3Fe7DD101g0M7OCalJN9u675sQAJFslGCjvs74PTOfEKt_t',
                                    'https://www.youtube.com/watch?v=n9DwoQ7HWvI')
 
 welcome = media.Movie('Welcome',
-                      'A comedy film in which a brother who is a underworld don want her sister to marry a well stablised guy.',
+                      'A brother who is a underworld don want her sister to marry a well stablised guy.',
                       'http://bharat-movies.com/hindi/img/MovieImg/Welcome-2007-MovieImg.jpg',
                       'https://www.youtube.com/watch?v=QJCMdzuLG0I')
 
 inception = media.Movie('Inception',
-                        'A thief with the rare ability to enter people\'s dreams and steal their secrets from their subconscious.',
+                        'A thief with the rare ability to enter people\'s dreams.',
                         'http://t2.gstatic.com/images?q=tbn:ANd9GcRo9vfJCM6dzPkZHIHBVCtlJnAnew9Ai26kEdrli0-tfmatmciD',
                         'https://www.youtube.com/watch?v=8hP9D6kZseM')
 
 harry_potter = media.Movie('Harry Potter and the deathly hallows',
-                           'Harry , Ron and Hermione begin a mission to destroy the Horcruxes, the sources of Voldemort\'s immortality.',
+                           'Harry , Ron and Hermione begin a mission to destroy the Horcruxes',
                            'http://t3.gstatic.com/images?q=tbn:ANd9GcTobkzVSJU5oZ9Pv_6bM0_o4RF_zFA9jyNwdHATG90vKxyhOk5x',
                            'https://www.youtube.com/watch?v=_EC2tmFVNNE')
 
 dawn_o_t_p_o_apes = media.Movie('Dawn of the Planet of the Apes',
-                                'a small band of human forces Caesar (leader of apes family) to grapple with the dual challenge of protecting his people and re-establishing a relationship with the remaining human population -- the latter being Caesar\'s secret wish.',
+                                'Caesor was offered protection of his people and re-establishion of a relationship with humans.',
                                 'http://t1.gstatic.com/images?q=tbn:ANd9GcRiFADcYKbJaffawZIolcpNA5aThmKHVIe3U8lnZCtsiqYyT51h',
                                 'https://www.youtube.com/watch?v=3sHMCRaS3ao')
 
 movie = [spiderman_homecoming, welcome, inception, harry_potter, dangal, dawn_o_t_p_o_apes]
+# Movie list contains all out movie instances and passed to open_movies_page method.
 fresh_tomatoes.open_movies_page(movie)
