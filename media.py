a69316c73406e472792266631173c80178f92f35
Siddharth Shorya <shoryasiddharth90@gmail.com>
2017-07-12 00:31:20 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,6 +1,7 @@
 import webbrowser
-# creating a class movie to store all related data 
+# creating a class movie to store all related data
 # of a movie that we wish to display.
+
 
 class Movie:
 
@@ -11,7 +12,7 @@ class Movie:
         self.storyline = m_storyline
         self.poster_image_url = m_poster
         self.trailer_youtube_url = youtube_trailer
-    
+
     def trailer(self):
         # method trailer displays the movie trailer video from youtube.
         webbrowser.open('self.trailer_youtube_url')
